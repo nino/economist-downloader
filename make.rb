75617d569e79013d10d4811c5786c356a9642418
Nino Annighöfer <nino@ninoan.com>
2017-06-15 23:15:14 +0200

    Make it put the output files in subdirectories

--- make.rb
+++ make.rb
@@ -1,10 +1,12 @@
 date = Time.now.strftime '%Y-%m-%d'
+system 'mkdir -p tmp'
+system 'mkdir -p issues'
 puts 'Getting stuff from the browser now ...'
-system "ruby economist-downloader.rb > economist-#{date}.html"
+system "ruby economist-downloader.rb > tmp/economist-#{date}.html"
 puts 'Stuff has been gotten from the browser.'
 puts 'Processing some stuff now ...'
-system "cat economist-#{date}.html | ruby process.rb > economist-#{date}-processed.html"
+system "cat tmp/economist-#{date}.html | ruby process.rb > tmp/economist-#{date}-processed.html"
 puts 'Stuff has been processed.'
 puts 'Turning it into an ebook now ...'
-system "pandoc -s economist-#{date}-processed.html -o economist-#{date}.epub"
+system "pandoc -s tmp/economist-#{date}-processed.html -o issues/economist-#{date}.epub"
 puts 'And we are done, theoretically.'
